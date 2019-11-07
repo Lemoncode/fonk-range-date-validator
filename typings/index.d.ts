1ef73dbf15b3d050a4e19315df03b0d73845311a
nasdan <daniel.sanchez@lemoncode.net>
2019-11-07 10:49:25 +0100

    update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,16 @@
 import { FieldValidationFunctionSync } from '@lemoncode/fonk';
 
 export namespace rangeDate {
-  export const validator: FieldValidationFunctionSync;
+  export interface CustomArgs {
+    min: Limit;
+    max: Limit;
+    parseStringToDateFn?: (value: string) => Date;
+  }
+  export interface Limit {
+    value: Date;
+    inclusive?: boolean;
+  }
+  export const validator: FieldValidationFunctionSync<CustomArgs>;
   export function setErrorMessage(message: string | string[]): void;
+  export function setCustomArgs(customArgs: Partial<CustomArgs>): void;
 }
